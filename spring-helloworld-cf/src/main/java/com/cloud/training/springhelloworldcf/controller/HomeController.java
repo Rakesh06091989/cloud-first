9ab5d9809ac6192a48004bdcf68679907085d339
Rakesh06091989 <rakesh.thumar@gmail.com>
2017-09-14 23:12:05 +0530

    first commit

--- spring-helloworld-cf/src/main/java/com/cloud/training/springhelloworldcf/controller/HomeController.java
+++ spring-helloworld-cf/src/main/java/com/cloud/training/springhelloworldcf/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.cloud.training.springhelloworldcf.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cloud.training.springhelloworldcf.model.GlobalProperties;
+
+
 
 @RestController
 public class HomeController {
 	
+	private GlobalProperties global;
+	
+    @Autowired
+    public void setGlobal(GlobalProperties global) {
+        this.global = global;
+    }
+    
 	@RequestMapping("/")
 	public String home() {
-		return "Das Boot, reporting for duty!";
+		return "Das Boot, reporting for duty!"+ global.toString();
 	}
 
 }
